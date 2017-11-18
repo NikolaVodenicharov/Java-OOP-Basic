ff48319958041a75b3a2e073ae6bf6627bc5b5b1
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-11-18 14:13:01 +0200

    Exercise - ValidationData

---
+++ Encapsulation/ValidationData/src/Main.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Person> people = readPeople(reader);
+        increasePeopleSalary(reader, people);
+        String formatted = toStringCollection(people);
+        System.out.println(formatted);
+    }
+
+    private static List<Person> readPeople(BufferedReader reader) throws IOException {
+        int inputLineNumber = Integer.parseInt(reader.readLine());
+
+        List<Person> people = new ArrayList<>(inputLineNumber);
+
+        for (int i = 0; i < inputLineNumber; i++) {
+            try{
+                String[] splitInput = reader.readLine().split(" ");
+                // is splitInput containing 4 elements
+
+                String firstName = splitInput[0];
+                String lastName = splitInput[1];
+                Integer age = Integer.parseInt(splitInput[2]);
+                Double salary = Double.parseDouble(splitInput[3]);
+
+                Person person = new Person(firstName, lastName, age, salary);
+                people.add(person);
+            }
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return people;
+    }
+    private static void increasePeopleSalary (BufferedReader reader, List<Person> people) throws IOException {
+        int bonus = Integer.parseInt(reader.readLine());
+        for (Person person : people) {
+            person.increaseSalary(bonus);
+        }
+    }
+    private static <E> String toStringCollection (List<E> collection){
+        StringBuilder formated = new StringBuilder();
+
+        // null elements - exception ?
+        for (E element : collection) {
+            formated
+                    .append(element)
+                    .append(System.lineSeparator());
+        }
+
+        return formated.toString().trim();
+    }
+}
